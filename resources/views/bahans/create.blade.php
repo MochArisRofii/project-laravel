5fc6af2e6dadeaa965ab66013cd47ea038b8192f
MochArisRofii <mochrofii827@gmail.com>
2024-07-27 13:48:03 +0700

    vo

--- resources/views/bahans/create.blade.php
+++ resources/views/bahans/create.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
     <title>Tambah Bahan</title>
     <style>
         body {
--- resources/views/reseps/edit.blade.php
+++ resources/views/reseps/edit.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Resep</title>
     {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
     <style>
         body {
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
